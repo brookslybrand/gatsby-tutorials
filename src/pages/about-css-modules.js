ae04ec736fffd94f0802bccac92c44c0a95facb9
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-06-14 18:14:03 -0500

    Add some css modules

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { Link } from "gatsby"
+import styles from "./about-css-modules.module.css"
+
+import Container from "../components/container"
+import Header from "../components/header"
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
+export default function About() {
+  return (
+    <Container>
+      <Link to="../">Home</Link>
+      <Header headerText="About Css Modules" />
+      <p>CSS Modules are cool</p>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </Container>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
+      <br />
+      <Link to="/about-css-modules/">About CSS Modules</Link>
       <Header>Hello Gatsby!</Header>
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />

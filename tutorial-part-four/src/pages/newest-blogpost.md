6f00cf36f5c6e04e486047d9f2393177d67982b1
Brooks Lybrand <brooks.lybrand@business-laboratory.com>
2020-06-14 17:45:56 -0500

    Remove all old tutorials

--- tutorial-part-four/src/pages/newest-blogpost.md
+++
@@ -1,6 +0,0 @@
----
-title: "Newest Blogpost"
-date: "2019-09-24"
----
-
-I don't write a lot of blog posts, but this was part of a challenge
--- tutorial-part-four/src/utils/typography.js
+++
@@ -1,5 +0,0 @@
-import Typography from "typography"
-import kirkhamtheme from "typography-theme-kirkham"
-const typography = new Typography(kirkhamtheme)
-export default typography
-export const rhythm = typography.rhythm
